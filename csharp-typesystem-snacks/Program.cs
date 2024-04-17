ad38ca6fd2f048be45204c3ba8fc765160667823
Fonta97 <fontana.alessandro97@gmail.com>
2024-04-17 14:33:17 +0200

    Snack 2 fatto :)

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Snack 1");
             Console.WriteLine("Dammi due numeri e ti dirò il maggiore!");
             Console.WriteLine("Numero 1: ");
             decimal number1 = decimal.Parse(Console.ReadLine());
@@ -11,11 +12,28 @@
             decimal number2 = decimal.Parse(Console.ReadLine());
             if (number1 < number2)
             {
-            Console.WriteLine($"Il numero maggiore è {number2}");
+                Console.WriteLine($"Il numero maggiore è {number2}");
             } else
             {
                 Console.WriteLine($"Il numero maggiore è {number1}");
             }
+
+            Console.WriteLine("Snack 2");
+            Console.WriteLine("Dammi due parole e ti dirò la più lunga! ;P");
+            Console.WriteLine("Parola 1: ");
+            string parola1 = Console.ReadLine();
+            Console.WriteLine("Parola 2: ");
+            string parola2 =  Console.ReadLine();
+            if (parola1.Length < parola2.Length)
+            {
+                Console.WriteLine($"La parola più lunga è {parola2}");
+            } else
+            {
+                Console.WriteLine($"La parola più lunga è {parola1}");
+            }
+
+
+            
         }
     }
 }
