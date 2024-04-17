6e9bfe0e4163807726104fd1362f9dff31953007
Fonta97 <fontana.alessandro97@gmail.com>
2024-04-17 14:22:43 +0200

    snack 1 fatto

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Dammi due numeri e ti dirò il maggiore!");
+            Console.WriteLine("Numero 1: ");
+            decimal number1 = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Numero 2: ");
+            decimal number2 = decimal.Parse(Console.ReadLine());
+            if (number1 < number2)
+            {
+            Console.WriteLine($"Il numero maggiore è {number2}");
+            } else
+            {
+                Console.WriteLine($"Il numero maggiore è {number1}");
+            }
         }
     }
 }
